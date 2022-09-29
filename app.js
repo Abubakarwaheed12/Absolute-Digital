5ed8792794f42d4ee38f120ccb573d72e527de03
abu <abubakarwaheed6346528@gmail.com>
2022-09-29 20:25:21 +0500

    edit some things in design

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-<script>
-    $('document').ready(function() {
-        $("#btn").click(function(){
-            $("#para").hide(100);
-        })
-    })
-</script>
+        // ------ACCORDIANS
+        var acc = document.getElementsByClassName("accordian");
+        var i;
+
+        for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+                if (panel.style.display === "block") {
+                    panel.style.display = "none";
+                } else {
+                    panel.style.display = "block";
+
+                }
+            });
+        }
+
+    // ---------------JS SCRIPT-----------------
+
+        //   menu 
+        $(document).ready(function () {
+            $(".toggle").click(function () {
+                // $(".menu.active").toggleClass("active");
+                $(".menu").toggleClass("active1");
+                $(".toggle").toggleClass("active2");
+            })
+
+            // ---------COUNTERS
+
+            $('.Count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 1000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+        });
+          });
+        });
+
+  
+
